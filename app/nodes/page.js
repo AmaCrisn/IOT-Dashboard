adf6a6c5d1444dfc49a2042d81beb26c9d15de91
AmaCrisn <galapersada9@gmail.com>
2025-05-27 14:33:11 +0700

    Added nodes for each item

--- app/nodes/page.js
+++ app/nodes/page.js
@@ -1,8 +1,14 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 import styles from "../page.module.css";
 import Link from "next/link";
+import WaterTank from "../water-tank/page";
 
-export default function SumpPit() {
+export default function Nodes() {
+  const [showPopup, setShowPopup] = useState(false);
+
   return (
     <div className="p-4 sm:p-6">
       <div>
@@ -112,8 +118,53 @@ export default function SumpPit() {
       </details>
 
       {/* <!-- Contents --> */}
-      <div className="grid justify-center items-center bg-white p-4 sm:p-8">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 justify-center items-center p-4 sm:p-8">
+        {/* Item */}
+        <div className="flex flex-col justify-center gap-4 shadow-md p-5 rounded-md">
+          <div className="flex flex-col gap-2 w-full">
+            {/* Nodes name / ID */}
+            <div className="flex justify-center font-bold uppercase text-md sm:text-lg">
+              <span className="truncate">AC2FCDC2DDBC</span>
+              {/* Alarm ping */}
+              <span className="relative flex h-2 w-2">
+                <span className="absolute inline-flex h-2 w-2 rounded-full bg-red-400 opacity-75 animate-ping"></span>
+                <span className="relative inline-flex h-2 w-2 rounded-full bg-red-400"></span>
+              </span>
+            </div>
 
+            {/* Location details */}
+            <div className="text-sm flex flex-col">
+              <h3>Royale Tower</h3>
+              <h3>Basement</h3>
+            </div>
+          </div>
+
+          <button onClick={() => setShowPopup(true)} className="bg-green-300 text-white py-1 rounded-lg">
+            Details
+          </button>
+
+          {showPopup && <WaterTank onClose={() => setShowPopup(false)} />}
+        </div>
+
+        {/* Item */}
+        <div className="flex flex-col justify-center gap-4 shadow-md p-5 rounded-md">
+          <div className="flex flex-col gap-2 w-full">
+            {/* Nodes name / ID */}
+            <div className="flex justify-center font-bold uppercase text-md text-md sm:text-lg">
+              <span className="truncate">4C27CCC2DDBC</span>
+            </div>
+
+            {/* Location details */}
+            <div className="text-sm flex flex-col">
+              <h3>Jasmine Tower</h3>
+              <h3>Basement</h3>
+            </div>
+          </div>
+
+          <button className="bg-green-300 text-white py-1 rounded-lg">
+            Details
+          </button>
+        </div>
       </div>
     </div>
   );
