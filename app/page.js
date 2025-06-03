ad7da138893570ac10048bc57789d70251a57d9a
AmaCrisn <galapersada9@gmail.com>
2025-06-03 13:55:52 +0700

    Fixed page icon

--- app/page.js
+++ app/page.js
@@ -13,29 +13,27 @@ export default function Home() {
           </h1>
 
           <div className="flex flex-col sm:flex-row px-5 gap-2 sm:gap-6">
-            <div className="h-12 w-12 rounded-md bg-green-300 shadow-md">
+            <div className="h-12 w-12 rounded-md bg-green-300 duration-300 hover:bg-green-400 shadow-md">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-arrow-up-right"
                 viewBox="0 0 16 16"
                 className="w-full h-full text-white p-1"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
                 />
               </svg>
             </div>
-            <div className="h-12 w-12 rounded-md bg-green-300 shadow-md">
+            <div className="h-12 w-12 rounded-md bg-green-300 duration-300 hover:bg-green-400 shadow-md">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-trash"
                 viewBox="0 0 16 16"
                 className="w-full h-full text-white p-1.5"
               >
